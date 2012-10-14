2215cbac976258b628533be0ec9422486dcb44f0
Chris Foster <cdbfoster@gmail.com>
2012-10-14 09:32:56 -0400

    Start on the state system

---
+++ source/recraft/core/StateRequest.java
@@ -0,0 +1,41 @@
+/********************************************************************************
+ *                                                                              *
+ *  This file is part of Recraft.                                               *
+ *                                                                              *
+ *  Recraft is free software: you can redistribute it and/or modify             *
+ *  it under the terms of the GNU General Public License as published by        *
+ *  the Free Software Foundation, either version 3 of the License, or           *
+ *  (at your option) any later version.                                         *
+ *                                                                              *
+ *  Recraft is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ *  GNU General Public License for more details.                                *
+ *                                                                              *
+ *  You should have received a copy of the GNU General Public License           *
+ *  along with Recraft.  If not, see <http://www.gnu.org/licenses/>.            *
+ *                                                                              *
+ *  Copyright 2012 Chris Foster.                                                *
+ *                                                                              *
+ ********************************************************************************/
+
+package recraft.core;
+
+import java.io.Serializable;
+
+public abstract class StateRequest implements Serializable
+{
+	private static final long serialVersionUID = -1995421321269565361L;
+
+	protected int requestedTick;
+
+	public StateRequest(int requestedTick)
+	{
+		this.requestedTick = requestedTick;
+	}
+
+	public int getRequestedTick()
+	{
+		return this.requestedTick;
+	}
+}
